e46f358011e26c7fa54b36dd3f5f77b1faa19f97
Jason Record <g23guy@gmail.com>
2013-12-23 12:18:01 -0700

    initial import

---
+++ Makefile
@@ -0,0 +1,56 @@
+OBSPACKAGE=sca-appliance-overlay
+VERSION='1.0'
+DATE=$(shell date '+%y%m%d-%H%M')
+SRCDIR=$(OBSPACKAGE)-$(VERSION)
+OVERLAY_FILE=$(OBSPACKAGE)-$(DATE).tar.gz
+
+default: overlay
+
+overlay: install
+	@echo overlay: Creating overlay tar ball
+	@cp COPYING.GPLv2 $(SRCDIR)
+	@cd $(SRCDIR); tar zcf ../$(OVERLAY_FILE) *
+	@echo; ls -l $$LS_OPTIONS; echo
+
+install: uninstall
+	@echo install: Creating overlay file directory structure
+	@install -d -m 755 -o root -g root \
+		$(SRCDIR)/etc/apache2/vhosts.d \
+		$(SRCDIR)/etc/init.d \
+		$(SRCDIR)/etc/sysconfig \
+		$(SRCDIR)/srv/ftp \
+		$(SRCDIR)/usr/lib/systemd/system
+	@install -d -m 766 -o ftp -g ftp \
+		$(SRCDIR)/srv/ftp/upload
+	@install -d -m 700 -o root -g root \
+		$(SRCDIR)/root
+	@echo install: Installing files
+	@install -m 600 -o root -g root overlay/vsftpd.conf $(SRCDIR)/etc/ 
+	@install -m 644 -o root -g root overlay/bash.bashrc.local $(SRCDIR)/etc/
+	@install -m 644 -o root -g root overlay/apache2 $(SRCDIR)/etc/sysconfig/
+	@install -m 644 -o root -g root overlay/after-local.service $(SRCDIR)/usr/lib/systemd/system/
+	@install -m 544 -o root -g root overlay/after.local $(SRCDIR)/etc/init.d/
+	@install -m 644 -o root -g root overlay/sca.conf $(SRCDIR)/etc/apache2/vhosts.d/
+	@install -m 644 -o root -g root \
+		overlay/.bashrc \
+		overlay/.first_boot_sca \
+		overlay/.last_boot_sca \
+		$(SRCDIR)/root/
+
+uninstall:
+	@echo uninstall: Uninstalling overlay file directory structure
+	@rm -rf $(SRCDIR)
+
+clean: uninstall
+	@echo clean: Cleaning up make files
+	@rm -f $(OBSPACKAGE)*
+	@echo; ls -l $$LS_OPTIONS; echo
+
+help:
+	@clear
+	@make -v
+	@echo
+	@echo Make options for file overlay: $(OBSPACKAGE)
+	@echo make {overlay[default], install, uninstall, clean}
+	@echo; ls -l $$LS_OPTIONS; echo
+
---
+++ scripts/Configuration-Scripts-Build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -e
+#
+# This script is executed at the end of appliance creation.  Here you can do
+# one-time actions to modify your appliance before it is ever used, like
+# removing files and directories to make it smaller, creating symlinks,
+# generating indexes, etc.
+#
+# The 'kiwi_type' variable will contain the format of the appliance
+# (oem = disk image, vmx = VMware, iso = CD/DVD, xen = Xen).
+#
+DPASS='linux'
+
+# read in some variables
+. /studio/profile
+
+# read in KIWI utility functions
+. /.kconfig
+
+#======================================
+# Creating Self-Signed HTTPS Keys
+#--------------------------------------
+openssl req -new -x509 -days 365 -keyout /etc/apache2/ssl.key/sca.key -out /etc/apache2/ssl.crt/sca.crt -nodes -subj  '/O=SCA Appliance/OU=Supportconfig Analysis Appliance/CN=localhost'
+
+#======================================
+# Configure default services
+#--------------------------------------
+# Services to turn on
+for i in 'apache2.service' 'vsftpd.service' 'sshd.service' 'after-local.service'
+do
+    echo "* Enabling systemd Service: $i "
+    systemctl enable $i
+done
+for i in mysql
+do
+    echo "* Enabling SysV Service: $i "
+    chkconfig $i on
+done
+
+#======================================
+# MySQL Configuration
+#--------------------------------------
+echo "* Starting MySQL "
+/etc/init.d/mysql start
+sleep 2
+/usr/bin/mysqladmin -u root password $DPASS
+echo "* MySQL Password Set "
+sleep 1
+
+#======================================
+# Configure Supportconfig Diagnostic Tool
+#--------------------------------------
+USE_HOSTNAME='localhost'
+
+/usr/sbin/setup-sca -bcfp $DPASS -s $USE_HOSTNAME
+sed -i -e 's!0:0:root:/!0:0:SCA Notification:/!g' /etc/passwd
+
+echo "* Creating SCA Links "
+cd /usr/local/sbin
+for i in /opt/scdiag/bin/*
+do
+  ln -sf $i
+done
